9c36caef219ab06c720f49570f4c6c051ad89a18
matusalik <mateuszsalik@gmail.com>
2025-05-14 22:09:21 +0200

    Refactor Examination entity for development

--- Przychodnia/Model/Examination.cs
+++ Przychodnia/Model/Examination.cs
@@ -9,23 +9,23 @@ namespace Przychodnia.Model;
 
 public class Examination
 {
-    public int Id { get; set; }
-    [Required]
-    public ExaminationType ExaminationType { get; set; }
+    public int? Id { get; set; }
+    //[Required]
+    public ExaminationType? ExaminationType { get; set; }
 
-    [Required]
-    public int PatientId { get; set; }
-    public Patient Patient { get; set; }
+    //[Required]
+    public int? PatientId { get; set; }
+    public Patient? Patient { get; set; }
 
-    [Required]
-    public int OrderedById { get; set; }
-    public User OrderedBy { get; set; }
+    //[Required]
+    public int? OrderedById { get; set; }
+    public User? OrderedBy { get; set; }
 
-    public int PerformingDoctorId { get; set; }
-    public User PerformingDoctor { get; set; }
+    public int? PerformingDoctorId { get; set; }
+    public User? PerformingDoctor { get; set; }
 
-    public int PerformingLaboratoryId { get; set; }
-    public Laboratory PerformingLaboratory { get; set; }
+    public int? PerformingLaboratoryId { get; set; }
+    public Laboratory? PerformingLaboratory { get; set; }
 }
 
 public enum ExaminationType
