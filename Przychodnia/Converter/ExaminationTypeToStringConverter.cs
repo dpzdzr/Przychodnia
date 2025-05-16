e2314f9b79a571afe5e81415188e4632d53b1e69
matusalik <mateuszsalik@gmail.com>
2025-05-16 10:41:51 +0200

    Added ExaminationTypeToStringConverter, ExaminationType shows properly in data grid

---
+++ Przychodnia/Converter/ExaminationTypeToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using Przychodnia.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Przychodnia.Converter;
+internal class ExaminationTypeToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is ExaminationType type)
+        {
+            return type == ExaminationType.Physical ? "Fizyczne" : "Laboratoryjne";
+        }
+        return "";
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is string typeString)
+        {
+            return typeString == "Fizyczne" ? ExaminationType.Physical : ExaminationType.Laboratory;
+        }
+        return "";
+    }
+}
